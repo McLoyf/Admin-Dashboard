3b40e808a9ae565394c3fa9f2e93d1eed6a60eac
McLoyf <alex.ortiz242005@gmail.com>
2026-01-29 11:06:09 -0600

    Use JavaScript to fill notification bell on click
    I did this because when I implemented the star, the bell notification stopped working for some reason

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const star = document.getElementById('star');
+const bell = document.getElementById('bell');
 
 star.addEventListener('click', function() {
     star.classList.toggle("filled");
+});
+
+bell.addEventListener('click', function() {
+    bell.classList.toggle("filled");
 });
